fa8ab27ca5cbfd2dbafb810b0ae1e11a159181a9
kafm <33590879+kafmws@users.noreply.github.com>
2020-05-26 21:17:40 +0800

    Initial commit

---
+++ src/main/java/com/xupt/outpatientms/OutpatientmsApplication.java
@@ -0,0 +1,13 @@
+package com.xupt.outpatientms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutpatientmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutpatientmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xupt/outpatientms/OutpatientmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xupt.outpatientms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutpatientmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
