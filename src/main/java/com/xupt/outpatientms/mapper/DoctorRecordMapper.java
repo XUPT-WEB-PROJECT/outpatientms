600d9ca1585c02b5c0099410a09d06405056a39e
WaterAndFreedom <2502644175@qq.com>
2020-06-04 01:03:44 +0800

    add doctorRecord function

---
+++ src/main/java/com/xupt/outpatientms/mapper/DoctorRecordMapper.java
@@ -0,0 +1,22 @@
+package com.xupt.outpatientms.mapper;
+
+import com.xupt.outpatientms.bean.Record;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import javax.xml.soap.SAAJResult;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by BorisLiu on 2020/6/3
+ */
+@Mapper
+public interface DoctorRecordMapper {
+
+    public Integer updateRecord(@Param("recordId") String recordId);
+
+    public Integer writeMedicalRecord(@Param("medicalRecord") String medicalRecord,@Param("recordId")String recordId,@Param("userId")String userId, @Param("doctorId")String doctorId);
+
+    public List<Record> getTodayAllRecord(@Param("doctorId")String doctorId,@Param("recordDate") String recordDate);
+}
