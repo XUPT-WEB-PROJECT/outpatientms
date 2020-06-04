16ad5440517fdfa2c1fed3610d7748d6affe339c
kafm <33590879+kafmws@users.noreply.github.com>
2020-06-04 16:19:37 +0800

    fix

--- src/main/java/com/xupt/outpatientms/bean/Department.java
+++ src/main/java/com/xupt/outpatientms/bean/Department.java
@@ -1,9 +1,12 @@
 package com.xupt.outpatientms.bean;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
@@ -11,8 +14,12 @@ import lombok.NoArgsConstructor;
 @ApiModel(description = "科室信息")
 public class Department {
 
+    @ApiModelProperty(name = "科室名称")
+    @NotNull(message = "科室名称不能为空")
     private String departmentName;
 
+    @ApiModelProperty(name = "科室位置不能为空")
+    @NotNull(message = "科室位置不能为空")
     private String departmentPos;
 
 }
